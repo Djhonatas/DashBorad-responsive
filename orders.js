1c0d2cfdc6eecef37e3ce1ce95571f3048d5b26d
Djhonatas Oliveira <dj.assuntospessoais@gmail.com>
2022-08-23 00:15:23 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders =[
+  {
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Foldable ',
+    productNumber: '.6546514',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Mini Drone',
+    productNumber: '641982',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: ' Drone',
+    productNumber: '6464',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Foldable  Drone',
+    productNumber: '8947641',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Foldable  Drone',
+    productNumber: '654',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Foldable Drone2',
+    productNumber: '125454',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+]
